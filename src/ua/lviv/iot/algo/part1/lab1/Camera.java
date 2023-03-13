0adba0303d0926b2ec1dfc687a790d48efebaa8b
meuf <solpo789@gmail.com>
2023-03-13 16:43:03 +0200

    change array creating

--- src/ua/lviv/iot/algo/part1/lab1/Camera.java
+++ src/ua/lviv/iot/algo/part1/lab1/Camera.java
@@ -1,5 +1,5 @@
 package ua.lviv.iot.algo.part1.lab1;
-//
+
 import lombok.*;
 
 @Getter
@@ -13,60 +13,45 @@ public class Camera {
     private double zoom;
     private String memoryCardType;
     private int photosCount;
-    private static Camera defaultPrinter = new Camera();
-    private static Camera instance= new Camera();
+
+    private static Camera instance = new Camera();
+
 
     public static Camera getInstance() {
-        return instance;
-    }
-
-    private Camera(String model) {
-        this.model = model;
-    }
-
-    public static Camera getInstance(String model) {
         if (instance == null) {
-            instance = new Camera(model);
+            instance = new Camera();
         }
         return instance;
     }
 
 
-    public void resetZoom()
-    {
+    public void resetZoom() {
         this.zoom = 1;
     }
-    public void savePhoto()
-    {
-        this.photosCount +=1;
+
+    public void savePhoto() {
+        this.photosCount += 1;
     }
-    public void eraseMemory(){
+
+    public void eraseMemory() {
         this.photosCount = 0;
     }
-    public void changeSettings(String resolution, double zoom){
+
+    public void changeSettings(String resolution, double zoom) {
         this.resolution = resolution;
         this.zoom = zoom;
-    }
-    @Override
-    public String toString() {
-        return "Camera " +
-                "model='" + model + '\'' +
-                ", resolution='" + resolution + '\'' +
-                ", zoom=" + zoom +
-                ", memoryCardType='" + memoryCardType + '\'' +
-                ", photosCount=" + photosCount;
     }
 
     public static void main(String[] args) {
         Camera[] cameras = {
-            new Camera("Canon", "1920x1080", 2.5, "SD", 0),
-            new Camera(),
-            getInstance(),
-            getInstance()
+                new Camera("Canon", "1920x1080", 2.5, "SD", 0),
+                new Camera(),
+                getInstance(),
+                getInstance()
         };
 
         for (Camera camera : cameras) {
-            System.out.println(camera.toString());
+            System.out.println(camera);
         }
     }
 }
